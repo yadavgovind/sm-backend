9cbb2eb366408436e25f16f1ca17d795fb264878
govind-sourceably <pradeep.maurya@sourceably.com>
2023-05-28 11:31:19 +0530

    uuuu

---
+++ src/main/java/com/sm/user/handler/LambdaHandler.java
@@ -0,0 +1,44 @@
+package com.sm.user.handler;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.time.Instant;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+import com.sm.user.SmUserApplication;
+
+public class LambdaHandler implements RequestStreamHandler {
+    private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+
+    static {
+        try {
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(SmUserApplication.class);
+            // For applications that take longer than 10 seconds to start, use the async builder:
+            long startTime = Instant.now().toEpochMilli();
+            handler = new SpringBootProxyHandlerBuilder()
+                    .defaultProxy()
+                    .asyncInit(startTime)
+                    .springBootApplication(SmUserApplication.class)
+                    .buildAndInitialize();
+        } catch (ContainerInitializationException e) {
+            // if we fail here. We re-throw the exception to force another cold start
+            e.printStackTrace();
+            throw new RuntimeException("Could not initialize Spring Boot application", e);
+        }
+    }
+
+    @Override
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+            throws IOException {
+        System.out.println("request in to the haldler");
+        System.out.println("request in to the haldler" + inputStream);
+        handler.proxyStream(inputStream, outputStream, context);
+    }
+}
